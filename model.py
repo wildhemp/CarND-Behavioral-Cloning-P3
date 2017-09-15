356f9ad98b202fb5d07a41263d82bd08ed215ccd
wildhemp <wildhemp@gmail.com>
2017-09-15 12:38:04 -0400

    Added new model and modified the augmentation

--- model.py
+++ model.py
@@ -4,13 +4,11 @@ import tensorflow as tf
 from sklearn.utils import shuffle
 import matplotlib.pyplot as plt
 import os
-from collections import Counter
 
-from keras.regularizers import l2, activity_l2
-from keras.layers import Dense, Flatten, Lambda, Conv2D, MaxPooling2D, Activation, Cropping2D, Dropout
+from keras.regularizers import l2
+from keras.layers import Dense, Flatten, Lambda, Conv2D, Activation, Cropping2D
 from keras.models import Sequential
 from keras.optimizers import Adam
-from keras.callbacks import EarlyStopping, ModelCheckpoint
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
@@ -23,22 +21,42 @@ flags.DEFINE_boolean('train', True, "Whether to train the model or not")
 
 
 def prepare_data(dataset, path, augment=False):
+    '''
+    Prepares the dataset. This constitutes of
+        * filtering out 2/3 of the 0 angles
+        * filtering out everything where speed is 0
+        * figuring out the path for the image files
+        * adding either all (when angle is > 0) or 1/3 of left and 1/3 of right camera images. The
+          correction angle applies is chosen by doing a back of the envelope calculation of how
+          much we'd need to add to the angle for the car to get back in the center image position
+          in ~2 secs assuming 30mph/50kph speed.
+
+    :param dataset: the dataset as it is loaded from the csv file
+    :param path: the path the images/csv are stored
+    :param augment: whether the dataset should be augmented with left/right images and filtered.
+        The validation dataset is not augmented.
+    :return: a tuple of (path, angle) for the (maybe) augmented/filtered data and a tuple for the
+        original data (to be used for e.g. visualization).
+    '''
     lines = []
     original = []
     for _, line in enumerate(dataset):
+        # leave out everything which has 0 speed.
         if float(line[6]) == .0:
             continue
 
         angle = float(line[3])
         image_path = path + 'IMG/' + line[0].decode('utf-8').split('/')[-1]
 
-        # store the original angles, so that we can visualize them and compare to the augmented dataset
+        # Store the original angles, so that we can visualize them and compare to the augmented
+        # dataset.
         original.append((image_path, angle))
 
         if not augment:
             lines.append((image_path, angle))
             continue
 
+        # There are too much data with 0 angle, so leave out 2/3 to not bias our model too much.
         if abs(angle) == .0 and np.random.uniform() < 2./3.:
             continue
 
@@ -46,11 +64,17 @@ def prepare_data(dataset, path, augment=False):
 
         include_both = abs(angle) > .0
 
+        # From back of the envelope calculation much we'd need to add to the angle for the car to
+        # get back in the center image position in ~2 secs assuming 30mph/50kph speed.
+        # The calculation actually says 0.075, but added a bit more so that it handles corners
+        # better.
         correction_angle = .09
 
         left_correct = min(1., angle + correction_angle)
         right_correct = max(-1., angle - correction_angle)
 
+        # If the angle is > 0, we include both left and right camera image to have a more balanced
+        # dataset.
         choice = np.random.uniform()
         if include_both or choice < 1./3.:
             image_path = path + 'IMG/' + line[1].decode('utf-8').split('/')[-1]
@@ -65,11 +89,21 @@ def prepare_data(dataset, path, augment=False):
 
 
 def load_data(path='data/'):
+    '''
+    Loads the data from the given path, split it to train and validation sets and prepares them
+    using #prepare_data(...). The validation set is simply the last 20% of the dataset. Since the
+    data is images and angles of driving this ensures that the validation dataset has the same
+    distribution as the whole dataset and the test track.
+
+    :param path: the path to load the data from. Defaults to 'data/'
+    :return: numpy arrays for the training, validation and the original dataset.
+    '''
     log = np.genfromtxt(path + 'driving_log.csv', delimiter=',', dtype=None, skip_header=1)
 
+    # Split the data to training/validation 80/20.
     size = len(log)
-    train = log[:int(size * .8 - .5)]
-    valid = log[int(size * .8 + .5):]
+    train = log[:int(size * .75 - .5)]
+    valid = log[int(size * .75 + .5):]
 
     train, train_orig = prepare_data(train, path, augment=True)
     valid, valid_orig = prepare_data(valid, path, augment=False)
@@ -78,21 +112,37 @@ def load_data(path='data/'):
 
 
 def dataset_info(dataset, title, column='angle', steps=0.02):
+    '''
+    Prints information and plots bar chart of the dataset.
+
+    :param dataset: the dataset to have the information about
+    :param title: the title of the chart
+    :param column: the column in the dataset to print the info about
+    :param steps: the steps for showing the bar chart
+    '''
     angles = dataset[column]
     max = np.max(angles)
     min = np.min(angles)
     print('{}: #: {} , Min: {:.4f}, Max: {:.4f}'.format(title, dataset.shape, min, max))
+
     plt.figure().canvas.set_window_title(title)
     plt.hist([angle for angle in angles if abs(angle) >= .0], bins=np.arange(min, max, steps))
 
 
 def visualize_data(train, valid, original):
+    '''
+    Visualizes the data, showing bar charts and sample images.
+
+    :param train: the train dataset
+    :param valid: the validation dataset
+    :param original: the original dataset
+    '''
     dataset_info(train, 'Training data')
     dataset_info(valid, 'Validation data')
     dataset_info(original, 'Original data')
 
     index = 6291 #np.random.randint(0, len(original))
-    print(index)
+
     center_path = original[index]['path'].decode("utf-8")
     path = center_path.split('/')
     right_path = os.path.join(path[0], path[1], 'right' + path[2][6:])
@@ -102,7 +152,7 @@ def visualize_data(train, valid, original):
     cv2.imshow('Center, angle: {:.4f}'.format(original[index]['angle']), image)
 
     image, angle = augment(image, original[index]['angle'])
-    cv2.imshow('Center (agumented), angle: {:.4f}'.format(angle), image)
+    cv2.imshow('Center (augmented), angle: {:.4f}'.format(angle), image)
 
     image = cv2.imread(right_path)
     cv2.imshow('Right, angle: {:.4f}'.format(original[index]['angle']), image)
@@ -110,15 +160,22 @@ def visualize_data(train, valid, original):
     image = cv2.imread(left_path)
     cv2.imshow('Left, angle: {:.4f}'.format(original[index]['angle']), image)
 
-    plt.show()
-
 
 def augment(image, angle):
+    '''
+    Augments the data by applying brightness, darkening and horizontal shifting to the image. The
+    code was partially taken from
+    https://github.com/jeremy-shannon/CarND-Behavioral-Cloning-Project/blob/747f21f02219e1743367fdc43aeb37993cafe08a/model.py#L89
+
+    :param image: the image to augment
+    :param angle: the angle belonging to the image
+    :return: the new image and angle
+    '''
     oh, ow = image.shape[:2]
 
     image = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
     image = np.array(image, dtype = np.float32)
-    # random brightness - the mask bit keeps values from going beyond (0,255)
+    # Random brightness - the mask bit keeps values from going beyond [0, 255]
     value = np.random.randint(-28, 28)
     if value > 0:
         mask = (image[:, :, 0] + value) > 255
@@ -126,7 +183,7 @@ def augment(image, angle):
         mask = (image[:, :, 0] + value) < 0
 
     image[:, :, 0] += np.where(mask, 0, value)
-    # random shadow - full height, random left/right side, random darkening
+    # Random shadow - full height, random left/right side, random darkening
     h, w = image.shape[0:2]
     mid = np.random.randint(0, w)
     factor = np.random.uniform(0.6, 0.8)
@@ -138,6 +195,10 @@ def augment(image, angle):
     image = np.array(image, dtype=np.uint8)
     image = cv2.cvtColor(image, cv2.COLOR_YUV2RGB)
 
+    # Random horizontal translation - when the angle is high, we make sure to translate, so that
+    # it's even higher, i.e. the car is turning even more. The assumption is, this will make the
+    # car not run out of the road on sharp turns. Otherwise we just randomly select a translation
+    # to either direction.
     if angle > .25:
         tx = np.random.uniform(0., 25.)
     elif angle < -.25:
@@ -146,13 +207,30 @@ def augment(image, angle):
         tx = np.random.uniform(-25., 25.)
 
     M = np.float32(([1, 0, tx], [0, 1, 0]))
+    image = cv2.warpAffine(image, M, (ow, oh))
+    angle += tx * .009
 
-    return cv2.warpAffine(image, M, (ow, oh)), angle + tx * .009
+    horizon = 2 * oh / 5
+    v_shift = np.random.randint(int(-oh / 8), int(oh / 8))
+    pts1 = np.float32([[0, horizon], [ow, horizon], [0, oh], [ow, oh]])
+    pts2 = np.float32([[0, horizon + v_shift], [ow, horizon + v_shift], [0, oh], [ow, oh]])
+    M = cv2.getPerspectiveTransform(pts1, pts2)
+    image = cv2.warpPerspective(image, M, (ow, oh), borderMode=cv2.BORDER_REPLICATE)
+
+    return image, angle
 
 
 def generator(data, bach_size=128):
+    '''
+    Generates the inputs for training the model.
+
+    :param data: the dataset to use (e.g. can be validation or training)
+    :param bach_size: the batch size.
+    :return: features, angles tuple
+    '''
     num_data = len(data)
     while True:
+        # Shuffle the data every time we start over, so that the order doesn't skew our model.
         data = shuffle(data)
         for offset in range(0, num_data, bach_size):
             batch = data[offset:offset + bach_size]
@@ -160,12 +238,16 @@ def generator(data, bach_size=128):
             images = []
             angles = []
             for current in batch:
+                # Read and conver the image from BGR to RGB. Interestingly, the model works quite
+                # well on RGB data when trained on BGR, but run of the road at some points...
                 image = cv2.cvtColor(cv2.imread(current['path'].decode("utf-8")), cv2.COLOR_BGR2RGB)
                 angle = float(current['angle'])
+                # Flip the images half the time, to help the model generalize better.
                 if np.random.uniform() < .5:
                     image = np.fliplr(image)
                     angle = -angle
 
+                # Apply random augmentation half the time, again, helping the model generalize.
                 if np.random.uniform() < .5:
                     image, angle = augment(image, angle)
 
@@ -178,9 +260,14 @@ def generator(data, bach_size=128):
 
 
 def build_model():
+    '''
+    Builds the model.
+
+    :return: the model
+    '''
     model = Sequential()
-    model.add(Cropping2D(cropping=((80, 30), (0, 0)), input_shape=(160, 320, 3)))
-    # normalize image
+    model.add(Cropping2D(cropping=((80, 30), (25, 25)), input_shape=(160, 320, 3)))
+    # Normalize image
     model.add(Lambda(lambda x: (x / 127.5) - 1.))
 
     model.add(Conv2D(32, 5, 5, subsample=(2, 2), border_mode='same', W_regularizer=l2(.001)))
@@ -212,8 +299,18 @@ def build_model():
     return model
 
 
+def visualize_loss(history):
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('Model MSE loss')
+    plt.ylabel('MSE loss')
+    plt.xlabel('Epoch')
+    plt.legend(['Training set', 'Validation set'], loc='upper right')
+
+
 def main(_):
     os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
     train_1, valid_1, original_1 = load_data()
     train_2, valid_2, original_2 = load_data(path='data_track_2/')
     train, valid, original = np.concatenate((train_1, train_2)), \
@@ -230,11 +327,14 @@ def main(_):
         visualize_data(train, valid, original)
 
     if FLAGS.train:
-        model.fit_generator(train_generator, samples_per_epoch=len(train), nb_epoch=FLAGS.epochs,
-                            validation_data=valid_generator, nb_val_samples=len(valid))
+        history = model.fit_generator(
+            train_generator, samples_per_epoch=len(train), nb_epoch=FLAGS.epochs,
+            validation_data=valid_generator, nb_val_samples=len(valid))
+        visualize_loss(history)
 
         model.save('model.h5')
 
+    plt.show()
 
 if __name__ == '__main__':
     tf.app.run()
--- writeup_report.md
+++ writeup_report.md
@@ -45,47 +45,91 @@ The model.py file contains the code for training and saving the convolution neur
 
 ####1. An appropriate model architecture has been employed
 
-My model consists of a convolution neural network with 3x3 filter sizes and depths between 32 and 64 (model.py lines 18-24) 
+My model consists of a convolution neural network with 5x5 and 3x3 filter sizes and depths between 32 and 64 and fully connected layers with feature sizes between 50-1000 (model.py lines 255-292)
 
-The model includes RELU layers to introduce nonlinearity (code line 20), and the data is normalized in the model using a Keras lambda layer (code line 18). 
+The model includes RELU layers to introduce nonlinearity, and the data is normalized and cropped in the model using a Keras lambda and cropping layers (model.py lines 262-264).
+
+The model is a somewhat modified version of the nvidia model, tuned to work better for the current problem.
 
 ####2. Attempts to reduce overfitting in the model
 
-The model contains dropout layers in order to reduce overfitting (model.py lines 21). 
+The model uses L2 regularization to prevent overfitting (e.g. model.py line 266). I also experimented with using Dropout layers, which worked, but using L2 made the loss decrease smoother (not jumping up and down), and the car run smoother on Track 1.
 
-The model was trained and validated on different data sets to ensure that the model was not overfitting (code line 10-16). The model was tested by running it through the simulator and ensuring that the vehicle could stay on the track.
+The model was trained and validated on different data sets (both track 1 and 2) to ensure that the model was not overfitting (code line 298-302).
+
+The model was tested by running it through the simulator and ensuring that the vehicle could stay on the track for both Track 1 and Track 2.
+
+I also modified the speed in drive.py to use the maximum, to see how well the model is able to keep up. It still drives both tracks well, although on Track 2 it gets very close to running off the road a couple times, but manages to (barely) stay on the the road.
 
 ####3. Model parameter tuning
 
-The model used an adam optimizer, so the learning rate was not tuned manually (model.py line 25).
+The model used an adam optimizer, so the learning rate was not tuned manually (model.py line 290).
+
+The L2 regularization is set to 0.001, which works quite well in preventing overfitting.
+
+The final model was trained for 5 epochs, which seemed to lead to the best result when testing on both tracks 1 and 2.
 
 ####4. Appropriate training data
 
-Training data was chosen to keep the vehicle driving on the road. I used a combination of center lane driving, recovering from the left and right sides of the road ... 
+Training data was chosen to keep the vehicle driving on the road. I used the udacity provided data, and recorded data from track 2 as well. For recovery the model was using the left and right images as well as horizontal translations (only necessary for the challange track).
 
-For details about how I created the training data, see the next section. 
+For details about how I created the training data, see the next section.
 
 ###Model Architecture and Training Strategy
 
 ####1. Solution Design Approach
 
-The overall strategy for deriving a model architecture was to ...
+The overall strategy for deriving a model architecture was to start with a somewhat simple model and modify it so that overfitting to a small data it results the lowest training loss possible then change it to prevent heavy overfitting.
 
-My first step was to use a convolution neural network model similar to the ... I thought this model might be appropriate because ...
+My first model consisted of 2 convolutional layers of 32 and 64 feature maps and 2 max pooling layer between them, all using strides of 2. This was followed by 3 fully connected layers of 400-100-1 features. This model has some similarity to LeNet, but uses larger feature maps for the convolutional layer to compensate for the larger input size.
 
-In order to gauge how well the model was working, I split my image and steering angle data into a training and validation set. I found that my first model had a low mean squared error on the training set but a high mean squared error on the validation set. This implied that the model was overfitting. 
+From this point I started adding/removing layers and changing feature maps/sizes so that the model results in lower training loss. I trained the model on a smaller dataset until the training loss started to increase again or just imply didn't decrease anymore, then repeated the process.
 
-To combat the overfitting, I modified the model so that ...
+I tried a couple different combinations and quickly arrived to a model very similar to the nvidia model. In the process I also got rid of the max pooling layers, because they didn't add any value anymore. Using strides of 2 on every layer reduced the size of the dataset anyways.
 
-Then I ... 
+The main difference in my model vs the nvidia model is:
 
-The final step was to run the simulator to see how well the car was driving around track one. There were a few spots where the vehicle fell off the track... to improve the driving behavior in these cases, I ....
+1. It starts 32 feature maps vs nvidia's 24. I tried 24, but it made things worth.
+2. It has larger fully connected layers, mainly to compensate the data size coming out of the convolutional layers.
+
+To combat the overfitting, I tried 2 strategies:
+
+1. Using dropout layers
+2. L2 regularization
+
+Using dropout layers worked quite well, but it made the training/validation loss jump around quite a lot, making it hard to judge where did the model start to overfit vs. it an still learn something. Also, the car seemed to be driving a little more erratically on track 1.
+
+So I gave a try to the L2 regularization and after tuning the data a little bit, it wokred very well. At the end I decided to use this instead of dropouts, because it was easier to figure out how many epochs to train and the car seemed to drive a little smoother.
+
+The final step was to run the simulator to see how well the car was driving around track one. There were a few spots where the model either drove off the road or got very close to it. To combat this the model was augmented with the left and right camera images and filtered, so that includes fewer data points with angle of 0.
+
+Also for track 2 additional augmentation was needed. Doing random horizontal translations made the model much better in staying on the road.
 
 At the end of the process, the vehicle is able to drive autonomously around the track without leaving the road.
 
 ####2. Final Model Architecture
 
-The final model architecture (model.py lines 18-24) consisted of a convolution neural network with the following layers and layer sizes ...
+The final model architecture (model.py lines 255-292) consisted of a convolution neural network with the following layers and layer sizes:
+
+
+|Layer                          | Description                                     |
+|:-----------------------------:|:-----------------------------------------------:|
+| Input                         | 160x320x3                                       |
+| Cropping2D                    | 50x320x3                                        |
+| Normalization (Lambda)        | 50x320x3                                        |
+| Convolution2D                 | 25x160x32 - 'same' padding, 2x2 strides, RELU, L2 0.001 |
+| Convolution2D                 | 13x80x48 - 'same' padding, 2x2 strides, RELU, L2 0.001 |
+| Convolution2D                 | 7x40x64 - 'same' padding, 2x2 strides, RELU, L2 0.001 |
+| Convolution2D                 | 4x20x64 - 'same' padding, 2x2 strides, RELU, L2 0.001 |
+| Dense                         | 1000 - RELU, L2 0.001                                 |
+| Dense                         | 250 - RELU, L2 0.001                                 |
+| Dense                         | 50 - RELU, L2 0.001                                 |
+| Dense                         | 1                                                   |
+
+
+Total params: 5,489,371
+Trainable params: 5,489,371
+
 
 Here is a visualization of the architecture (note: visualizing the architecture is optional according to the project rubric)
 
